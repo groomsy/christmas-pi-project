93fe130fd86b7b3c114b23c9a70da432f4b0599a
Todd Grooms <t@toddgrooms.com>
2020-12-06 08:00:43 -0600

    Update LEDPattern.py to control an additional LED
    Since the Xmas.py script expects eight LEDs, let me verify LEDPattern.py
    works with my eight LEDs first. Then I can just update Xmas.py to use
    the same pins.

--- LEDPattern.py
+++ LEDPattern.py
@@ -5,58 +5,30 @@
 # https://www.instructables.com/Raspberry-Pi-Christmas-Tree-Light-Show/
 #
 
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO, time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-time.sleep(2.0)
 
 gpio4 = 7
-GPIO.setup(gpio4, GPIO.OUT)
-GPIO.output(gpio4, True)
-time.sleep(2.0)
-GPIO.output(gpio4, False)
-
 gpio5 = 29
-GPIO.setup(gpio5,  GPIO.OUT)
-GPIO.output(gpio5, True)
-time.sleep(2.0)
-GPIO.output(gpio5, False)
-
 gpio6 = 31
-GPIO.setup(gpio6,  GPIO.OUT)
-GPIO.output(gpio6, True)
-time.sleep(2.0)
-GPIO.output(gpio6, False)
-
 gpio7 = 26
-GPIO.setup(gpio7,  GPIO.OUT)
-GPIO.output(gpio7, True)
-time.sleep(2.0)
-GPIO.output(gpio7, False)
-
 gpio8 = 24
-GPIO.setup(gpio8,  GPIO.OUT)
-GPIO.output(gpio8, True)
-time.sleep(2.0)
-GPIO.output(gpio8, False)
-
 gpio9 = 21
-GPIO.setup(gpio9,  GPIO.OUT)
-GPIO.output(gpio9, True)
-time.sleep(2.0)
-GPIO.output(gpio9, False)
-
 gpio10 = 19
-GPIO.setup(gpio10,  GPIO.OUT)
-GPIO.output(gpio10, True)
-time.sleep(2.0)
-GPIO.output(gpio10, False)
-
 gpio11 = 23
-GPIO.setup(gpio11,  GPIO.OUT)
-GPIO.output(gpio11, True)
-time.sleep(2.0)
-GPIO.output(gpio11, False)
+gpio12 = 32
 
+pinList = [gpio4, gpio5, gpio6, gpio7, gpio8, gpio9, gpio10, gpio11, gpio12]
+
+for gpioPin in pinList:
+    GPIO.setup(gpioPin, GPIO.OUT)
+    GPIO.output(gpioPin, False)
+
+time.sleep(2.0)
+
+for gpioPin in pinList:
+    GPIO.output(gpioPin, True)
+    time.sleep(2.0)
+    GPIO.output(gpioPin, False)
